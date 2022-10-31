331793fffba6f4bf5201fef551d2ed0a6b598509
Oscar <omarquezpayall@gmail.com>
2022-10-31 11:31:30 -0400

    Eliminar

--- payall/models/account_analytic_line.py
+++ payall/models/account_analytic_line.py
@@ -16,7 +16,7 @@ class AccountAnalyticLine( models.Model):
     @api.onchange('name')
     def task_notify_name(self):
         if self.task_id:
-            self.sudo().task_id._message_post( body = 
+            self.sudo().task_id.message_post( body = 
                 "El usuario " + self.env.user.display_name + " ha modificado la descripcion del registro de horas a " + self.name,
                 message_type = "comment"
             )
@@ -24,7 +24,7 @@ class AccountAnalyticLine( models.Model):
     @api.onchange('date')
     def task_notify_date(self):
         if self.task_id:
-            self.sudo().task_id._message_post( body = 
+            self.sudo().task_id.message_post( body = 
                 "El usuario " + self.env.user.display_name + " ha modificado la fecha del registro de horas a " + str(self.date),
                 message_type = "comment"
             )
@@ -32,7 +32,7 @@ class AccountAnalyticLine( models.Model):
     @api.onchange('time_spent')
     def task_notify_hours(self):
         if self.task_id:
-            self.sudo().task_id._message_post( body = 
+            self.sudo().task_id.message_post( body = 
                 "El usuario " + self.env.user.display_name + " ha modificado las horas realizadas del registro de horas a " + self.time_spent,
                 message_type = "comment"
             )
