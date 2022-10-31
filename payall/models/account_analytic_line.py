56dec0743356ab511680b7639ba9b56e1db44bae
Oscar <omarquezpayall@gmail.com>
2022-10-31 12:02:38 -0400

    Prueba log interno

--- payall/models/account_analytic_line.py
+++ payall/models/account_analytic_line.py
@@ -13,25 +13,19 @@ class AccountAnalyticLine( models.Model):
     )
 
 
-    @api.onchange('name')
-    def task_notify_name(self):
-        if self.task_id:
+    def write(self, vals):
+        result = super( AccountAnalyticLine, self).write(vals)
+        if vals['name'] and result:
             self.sudo().task_id.message_post( body = 
                 "El usuario " + self.env.user.display_name + " ha modificado la descripcion del registro de horas a " + self.name,
                 message_type = "comment"
             )
-    
-    @api.onchange('date')
-    def task_notify_date(self):
-        if self.task_id:
+        if vals['date'] and result:
             self.sudo().task_id.message_post( body = 
-                "El usuario " + self.env.user.display_name + " ha modificado la fecha del registro de horas a " + str(self.date),
+                "El usuario " + self.env.user.display_name + " ha modificado la descripcion del registro de horas a " + self.name,
                 message_type = "comment"
             )
-    
-    @api.onchange('time_spent')
-    def task_notify_hours(self):
-        if self.task_id:
+        if vals['time_spent'] and result:
             self.sudo().task_id.message_post( body = 
                 "El usuario " + self.env.user.display_name + " ha modificado las horas realizadas del registro de horas a " + self.time_spent,
                 message_type = "comment"
