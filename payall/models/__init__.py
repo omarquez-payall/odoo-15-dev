50ef755cb7a49151cdef8cde97f0deec83797cba
calviar-odoo <calviarpayall@gmail.com>
2023-01-05 15:10:18 -0400

    remove old files

--- payall/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import project_task
-from . import priority
-from . import peso
-from . import account_analytic_line
-from . import task_line_type
-from . import project_task_create_timesheet
